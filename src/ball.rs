3a3fd7196a32af77428bb909a9d3c52b883ac93a
Vianpyro <vianney@veremme.org>
2025-10-06 11:17:50 -0400

    Refactor ball physics to incorporate speed management

--- src/ball.rs
+++ src/ball.rs
@@ -3,10 +3,13 @@ use rand::Rng;
 
 pub const BALL_SPEED: f32 = 500.0;
 pub const BALL_SIZE: f32 = 20.0;
+pub const BALL_SPEED_INCREMENT: f32 = 1.05;
+pub const BALL_SPEED_MAX: f32 = BALL_SPEED * 5.0;
 
 pub struct Ball {
     pub position: Vec2,
     pub velocity: Vec2,
+    pub speed: f32,
     ball_mesh: graphics::Mesh,
 }
 
@@ -28,11 +31,12 @@ impl Ball {
         canvas.draw(&self.ball_mesh, graphics::DrawParam::default().dest(self.position));
     }
 
-    /// Reset ball position and randomize its velocity.
+    /// Reset ball position and speed, and randomize its direction.
     pub fn reset(&mut self, pos_x: f32, pos_y: f32) {
         self.position = Vec2::new(pos_x, pos_y);
-        randomize_velocity(&mut self.velocity, BALL_SPEED, BALL_SPEED);
-        self.velocity = self.velocity.normalize() * BALL_SPEED;
+        self.speed = BALL_SPEED;
+        randomize_velocity(&mut self.velocity, self.speed, self.speed);
+        self.velocity = self.velocity.normalize() * self.speed;
     }
 }
 
@@ -46,6 +50,7 @@ impl Ball {
         Ok(Ball {
             position: Vec2::new(pos_x, pos_y),
             velocity: ball_velocity.normalize() * BALL_SPEED,
+            speed: BALL_SPEED,
             ball_mesh,
         })
     }
